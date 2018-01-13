c805441cafc937076e2e35a09b8085e55cbea203
karolskora1993 <karol-skora@poczta.pl>
2018-01-13 16:24:26 +0100

    implement push notifications

---
+++ app/src/main/java/com/example/apple/facedetectionmobileapp/Constants.java
@@ -0,0 +1,12 @@
+package com.example.apple.facedetectionmobileapp;
+
+/**
+ * Created by apple on 13.01.2018.
+ */
+
+public class Constants {
+
+    public static final String CHANNEL_ID = "my_channel_01";
+    public static final String CHANNEL_NAME = "Push Notification";
+    public static final String CHANNEL_DESCRIPTION = "push notification from facedetectionapi.herokuapp.com";
+}
--- app/src/main/java/com/example/apple/facedetectionmobileapp/MainActivity.java
+++ app/src/main/java/com/example/apple/facedetectionmobileapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.apple.facedetectionmobileapp;
 
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -9,5 +13,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+//            NotificationManager mNotificationManager =
+//                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//            int importance = NotificationManager.IMPORTANCE_HIGH;
+//            NotificationChannel mChannel = new NotificationChannel(Constants.CHANNEL_ID, Constants.CHANNEL_NAME, importance);
+//            mChannel.setDescription(Constants.CHANNEL_DESCRIPTION);
+//            mChannel.enableLights(true);
+//            mChannel.setLightColor(Color.RED);
+//            mChannel.enableVibration(true);
+//            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+//            mNotificationManager.createNotificationChannel(mChannel);
+//        }
     }
 }
